25bab00706472cb4676c0de0f6b439904e220647
Anastasiia <nastyayavorska@gmail.com>
2022-08-04 00:34:18 +0200

    Add API to forecast

--- src/script.js
+++ src/script.js
@@ -50,6 +50,14 @@ function displayForecast () {
 forecastHTML = forecastHTML + `</div>`;
 forecastElement.innerHTML = forecastHTML;
 }
+
+function getForecast (coordinates) {
+  console.log(coordinates);
+  let apiKey = "da16704800751c14adceb19bcac00e36";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function changeCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#searchInput");
@@ -74,7 +82,7 @@ function showCurrentWeather(response) {
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
   let icon = document.querySelector("#icon");
-  icon.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+  icon.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 
 function getCurrentPosition(position) {
@@ -101,6 +109,7 @@ function displayWeather(response) {
   document.querySelector("#description").innerHTML =response.data.weather[0].description;
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#icon").setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+  getForecast(response.data.coord);
 }
 function searchCity(city) {
   let apiKey = "da16704800751c14adceb19bcac00e36";
@@ -227,4 +236,3 @@ function lookUpParis(response) {
   axios.get(apiUrl).then(displayWeather);
 }
 clickParis.addEventListener("click", lookUpParis);
-displayForecast();
